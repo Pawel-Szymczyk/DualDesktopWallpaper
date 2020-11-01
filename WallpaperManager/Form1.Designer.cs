b318a2c5c32c270b9b20cce02453597d03bd9405
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2020-11-01 21:00:25 +0000

    - search for wallpaper over the internet for selected screen - dispaly screen resolution for selecteed screen - merge tweo displays in the case we want have one wallpaper over two screens

--- WallpaperManager/Form1.Designer.cs
+++ WallpaperManager/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.label8 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // detectScreenBtn
@@ -141,12 +142,24 @@
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(507, 539);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 23);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "search for wallpaper";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(786, 786);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label8);
@@ -160,7 +173,7 @@
             this.Name = "Form1";
             this.Text = "DW";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,6 +190,7 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WallpaperManager/Form1.cs
+++ WallpaperManager/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -47,8 +48,6 @@ namespace WallpaperManager
             //new DropShadow().ApplyShadows(this);
 
 
-
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -74,6 +73,70 @@ namespace WallpaperManager
 
         // ------------------------------------------
         // class Display
+        
+        public void DrawDisplay()
+        {
+            var panelMiddlePointX = this.freeSpaceArea.Bounds.Width / 2;
+            var panelMiddlePointY = this.freeSpaceArea.Bounds.Height / 2;
+
+            Point realDisplayOneShiftLocation = new Point();
+
+            int scale = 21;
+            int margin = 1;
+
+            Screen screen = null;
+            if (Screen.AllScreens[0].Primary)
+            {
+                screen = Screen.AllScreens[0];
+            }
+            else
+            {
+                screen = Screen.AllScreens[1];
+            }
+
+
+
+            // actual screen scaled size
+            var scaledScreenHeight = screen.Bounds.Size.Height / scale;
+            var scaledScreenWidth = (screen.Bounds.Size.Width / scale);
+
+            // center of actual screen
+            var displayCenterX = scaledScreenWidth / 2;
+            var displayCenterY = scaledScreenHeight / 2;
+
+
+            var display = new PictureBox();
+            display.Name = screen.DeviceName.Replace(@"\.", "").Replace(@"\", "").ToLower();
+            display.Size = new Size(scaledScreenWidth, scaledScreenHeight);
+            display.BackColor = Color.LightGray;
+            display.BorderStyle = BorderStyle.FixedSingle;
+            display.Paint += new PaintEventHandler((sender, e) =>
+            {
+                e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+
+                string text = "1 | 2";
+                Font font = new Font("Segoe UI", 40);
+
+                SizeF textSize = e.Graphics.MeasureString(text, font);
+                PointF locationToDraw = new PointF();
+                locationToDraw.X = (display.Width / 2) - (textSize.Width / 2);
+                locationToDraw.Y = (display.Height / 2) - (textSize.Height / 2);
+
+                e.Graphics.DrawString(text, font, Brushes.White, locationToDraw);
+            });
+
+
+            realDisplayOneShiftLocation = new Point(panelMiddlePointX - displayCenterX, panelMiddlePointY - displayCenterY);
+
+            display.Location = realDisplayOneShiftLocation;
+            display.MouseDoubleClick += new MouseEventHandler(wallpaper1_DoubleClick);
+
+
+
+            this.freeSpaceArea.Controls.Add(display);
+        }
+
+
         private void DrawDisplays()
         {
             // panel area
@@ -102,7 +165,7 @@ namespace WallpaperManager
                 display.Size = new Size(scaledScreenWidth, scaledScreenHeight);
                 display.BackColor = Color.LightGray;
                 display.BorderStyle = BorderStyle.FixedSingle;
-                
+                display.Paint += new PaintEventHandler(wallpaper_Label);
 
                 if (screen.Primary)
                 {
@@ -110,6 +173,7 @@ namespace WallpaperManager
 
                     display.Location = realDisplayOneShiftLocation;
                     display.MouseDoubleClick += new MouseEventHandler(wallpaper1_DoubleClick);
+                    display.MouseClick += new MouseEventHandler((sender, e) => wallpaper_SingleClick(sender, e, Screen.AllScreens));
                 }
                 else
                 {
@@ -148,7 +212,7 @@ namespace WallpaperManager
 
                     display.Location = new Point(x, y);
                     display.MouseDoubleClick += new MouseEventHandler(wallpaper2_DoubleClick);
-
+                    display.MouseClick += new MouseEventHandler((sender, e) => wallpaper_SingleClick(sender, e, Screen.AllScreens));
                 }
 
                 
@@ -157,9 +221,82 @@ namespace WallpaperManager
             }
 
         }
+        private string wallpaperResolution;
+        private void wallpaper_SingleClick(object sender, MouseEventArgs e, Screen[] screens)
+        {
+            // change button visisbility to true
+            // add button event
+            // add screen refrences to button
+
+            
+
+            PictureBox pictureBox = (PictureBox)sender;
+
+            // remove any labels first
+
+            var dynamicLabels = this.freeSpaceArea.Controls.OfType<Label>().Where(c => c.Tag != null && c.Tag.ToString() == "resolution").ToList();
+
+            foreach (var lbl in dynamicLabels)
+            {
+                this.freeSpaceArea.Controls.Remove(lbl);
+            }
 
 
+            // add label to each click picturebox with its resoultion, remove old label
+            Label label = new Label();
+            label.Font = new Font("Segoe UI", 14);
+            label.Size = new Size(120, 20);
+            label.Location = new Point(pictureBox.Bounds.Location.X, pictureBox.Bounds.Location.Y -50);
+            label.Name = $"{pictureBox.Name}";
+            label.Tag = "resolution";
+            if (pictureBox.Name == "display1")
+            {
+                label.Text = $"{screens[0].Bounds.Width} x {screens[0].Bounds.Height}";
+                wallpaperResolution = $"{screens[0].Bounds.Width} x {screens[0].Bounds.Height}";
+            }
+            else if(pictureBox.Name == "display2")
+            {
+                label.Text = $"{screens[1].Bounds.Width} x {screens[1].Bounds.Height}";
+                wallpaperResolution = $"{screens[1].Bounds.Width} x {screens[1].Bounds.Height}";
+            }
+            freeSpaceArea.Controls.Add(label);
 
+            button1.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"https://www.google.com/search?q=wallpaper+" + wallpaperResolution);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.button1.Visible = false;
+
+            var dynamicLabels = this.freeSpaceArea.Controls.OfType<Label>().Where(c => c.Tag != null && c.Tag.ToString() == "resolution").ToList();
+
+            foreach (var lbl in dynamicLabels)
+            {
+                this.freeSpaceArea.Controls.Remove(lbl);
+            }
+        }
+
+        private void wallpaper_Label(object sender, PaintEventArgs e)
+        {
+            PictureBox display = (PictureBox)sender;
+
+            e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+
+            string text = display.Name.Replace(@"display", "");
+            Font font = new Font("Segoe UI", 40);
+
+            SizeF textSize = e.Graphics.MeasureString(text, font);
+            PointF locationToDraw = new PointF();
+            locationToDraw.X = (display.Width / 2) - (textSize.Width / 2);
+            locationToDraw.Y = (display.Height / 2) - (textSize.Height / 2);
+
+            e.Graphics.DrawString(text, font, Brushes.White, locationToDraw);
+        }
 
         private async Task<Form> CreateForm(Screen screen, int monitor)
         {
@@ -285,7 +422,7 @@ namespace WallpaperManager
         private void setWallpaperBtnEnabled()
         {
             if (!string.IsNullOrEmpty(this.wallpaperFileName1)
-               && !string.IsNullOrEmpty(this.wallpaperFileName2))
+               && !string.IsNullOrEmpty(this.wallpaperFileName2) || checkBox1.Checked)
             {
                 this.setWallpaperBtn.Enabled = true;
             }
@@ -394,46 +531,112 @@ namespace WallpaperManager
             return outputImage;
         }
 
+        private Bitmap StretchImageOverTwoDisplays(Image firstImage, Screen[] screens)
+        {
+            var firstScreen = screens[0];
+            var secondScreen = screens[1];
+
+            var width = firstScreen.Bounds.Width + secondScreen.Bounds.Width;
+            var height = firstScreen.Bounds.Height > secondScreen.Bounds.Height ? firstScreen.Bounds.Height : secondScreen.Bounds.Height;
+            //var height = 3390;
+
+            var outputImage = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            using (var graphics = Graphics.FromImage(outputImage))
+            {
+                graphics.Clear(Color.Transparent);
+                //graphics.DrawImage(firstImage, new Rectangle(new Point(), firstImage.Size), new Rectangle(new Point(), firstImage.Size), GraphicsUnit.Pixel);
+
+                graphics.DrawImage(firstImage, new Rectangle(0,0, width, height));
+            }
+
+                //var outputImageWidth = firstImage.Width + secondImage.Width;
+                //var outputImageHeight = firstImage.Height > secondImage.Height ? firstImage.Height : secondImage.Height;
+
+                //var outputImage = new Bitmap(outputImageWidth, outputImageHeight, PixelFormat.Format32bppArgb);
+
+                //// TODO : replace 0 and new point() with actual y position 
+
+                //    using (var graphics = Graphics.FromImage(outputImage))
+                //    {
+                //        graphics.Clear(Color.Transparent);
+                //        graphics.DrawImage(firstImage, new Rectangle(new Point(), firstImage.Size), new Rectangle(new Point(), firstImage.Size), GraphicsUnit.Pixel);
+                //        //graphics.DrawImage(secondImage, new Rectangle(new Point(firstImage.Width, 0), secondImage.Size), new Rectangle(new Point(), secondImage.Size), GraphicsUnit.Pixel);
+                //    }
+
+
+
+
+
+
+
+                return outputImage;
+        }
+
         // Merge images and store them in the windows registry.
         private void setWallpaperBtn_Click(object sender, EventArgs e)
         {
             if (!string.IsNullOrEmpty(this.wallpaperFileName1)
-                && !string.IsNullOrEmpty(this.wallpaperFileName2))
+                && !string.IsNullOrEmpty(this.wallpaperFileName2) || checkBox1.Checked)
             {
 
                 // merge images in bitmap
 
-                //foreach (var screen in Screen.AllScreens)
-                //{ 
-                //}
 
                 // Note: there is no need to save bitmap and images cos it is stored as register key
                 // without saving, setting background speed increases.
 
                 var img1 = Image.FromFile(this.wallpaperFileName1);
-                var img2 = Image.FromFile(this.wallpaperFileName2);
-                Bitmap bm = MergeTwoImages(img1, img2, Screen.AllScreens);
+                var img2 = !string.IsNullOrEmpty(this.wallpaperFileName2) ? Image.FromFile(this.wallpaperFileName2) : null;
+
+                Bitmap bm;
+
+                if (this.checkBox1.Checked)
+                {
+                    Console.WriteLine("hey");
+                    bm = StretchImageOverTwoDisplays(img1, Screen.AllScreens);
+                    SaveBackground(bm, "0", "1");
+                }
+                else
+                {
+                    bm = MergeTwoImages(img1, img2, Screen.AllScreens);
+                    SaveBackground(bm, "0", "1");
+                }
+
+
+                  
                 //bm.Save("result.png", ImageFormat.Png);
 
 
 
                 //System.Drawing.Image img = System.Drawing.Image.FromFile(@"F:\Projects\VStudio\WallpaperManager\WallpaperManager\bin\Debug\result.png");
 
-                // store temporary image in appdata...
-                var img = (Image)bm;
-                string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
-                img.Save(tempPath, ImageFormat.Bmp);
-
-
-
-                RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
-                key.SetValue(@"WallpaperStyle", 0.ToString());
-                key.SetValue(@"TileWallpaper", 1.ToString());
-                SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, tempPath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+               
 
 
             }
 
+        }
+
+        private void SaveBackground(Bitmap bitmap, string wallpaperStyle, string tileWallpaper)
+        {
+            // store temporary image in appdata...
+            var img = (Image)bitmap;
+            string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+            img.Save(tempPath, ImageFormat.Bmp);
+
+            // wallpaper styles:
+            // 0 - center
+            // 2 - streched
+            // 6 - fit
+            // 10 - fill
+
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            //key.SetValue(@"WallpaperStyle", 0.ToString());
+            //key.SetValue(@"TileWallpaper", 1.ToString());
+            key.SetValue(@"WallpaperStyle", wallpaperStyle);
+            key.SetValue(@"TileWallpaper", tileWallpaper);
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, tempPath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
         }
 
 
@@ -451,6 +654,41 @@ namespace WallpaperManager
 
 
 
+
+
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // remove displays and drow 1 common for both displays 
+            // allow to click on it
+
+            // in the case checkbox is unchecked it should display two displays
+
+            var x = freeSpaceArea.Controls.OfType<PictureBox>();
+
+            if (checkBox1.Checked)
+            {
+                //foreach (Control item in freeSpaceArea.Controls.OfType<PictureBox>())
+                //{
+                //    freeSpaceArea.Controls.Remove(item);
+                //    item.Dispose();
+                //}
+                freeSpaceArea.Controls.Clear();
+
+                DrawDisplay();
+
+            }
+            else
+            {
+                freeSpaceArea.Controls.Clear();
+                DrawDisplays();
+            }
+
+        }
+
+ 
+
         /// <summary>
         /// Close form.
         /// </summary>
@@ -459,47 +697,6 @@ namespace WallpaperManager
         //    this.Dispose();
         //    this.Close();
         //}
-
-        private void Form1_MouseDown(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                ReleaseCapture();
-                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
-            }
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            //var img1 = Image.FromFile(this.wallpaperFileName1);
-            //var img2 = Image.FromFile(this.wallpaperFileName2);
-            //Bitmap bm = MergeTwoImages(img1, img2, Screen.AllScreens);
-            ////bm.Save("result.png", ImageFormat.Png);
-
-
-
-            ////System.Drawing.Image img = System.Drawing.Image.FromFile(@"F:\Projects\VStudio\WallpaperManager\WallpaperManager\bin\Debug\result.png");
-
-            //// store temporary image in appdata...
-            //var img = (Image)bm;
-            //string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
-            //img.Save(tempPath, ImageFormat.Bmp);
-
-
-
-            //RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
-            //key.SetValue(@"WallpaperStyle", 0.ToString());
-            //key.SetValue(@"TileWallpaper", 1.ToString());
-            //SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, tempPath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
-        }
-
-
-
-
-
-
-
-
 
         // Explanation: 
         // I have removed function of drag and drop of the displays features to this program, because lack of 
